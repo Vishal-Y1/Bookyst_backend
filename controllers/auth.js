cd91ec14a2ab675c3184427110ccad84d374c6b7
Vishal Yadav <merndevvishal@gmail.com>
2024-10-23 12:02:39 +0530

    fixing cookie errors 1

--- controllers/auth.js
+++ controllers/auth.js
@@ -33,7 +33,7 @@ const signIn = async (req, res) => {
       .cookie("access_token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "None",
+
         maxAge: 7 * 24 * 60 * 60 * 1000,
       })
       .json({ ...others, token });
